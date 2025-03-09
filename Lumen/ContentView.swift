291ea87da9d74fdce210807c29faccbfcb1cc8f1
Aram Soneson <aram.soneson@icloud.com>
2025-03-08 19:36:04 -0500

    Initial Commit

---
+++ Lumen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lumen
+//
+//  Created by Aram Soneson on 3/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
